1953997531c2d3dffedadd31418a3d9b58891945
eiguzman <eiguzman@ucsd.edu>
2025-01-25 22:36:20 -0800

    updated global.js

--- global.js
+++ global.js
@@ -6,10 +6,10 @@ function $$(selector, context = document) {
 
 let pages = [
     { url: 'https://eiguzman.github.io/portfolio/', title: 'Home' },
-    { url: 'projects/index.html', title: 'Projects' },
-    { url: 'contact/index.html', title: 'Contact' },
+    { url: 'portfolio/projects/index.html', title: 'Projects' },
+    { url: 'portfolio/contact/index.html', title: 'Contact' },
     { url: 'https://github.com/eiguzman', title: 'GitHub' },
-    { url: 'contact/cv.html', title: 'CV' },
+    { url: 'portfolio/contact/cv.html', title: 'CV' },
   ];
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
@@ -20,6 +20,7 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+    url = !ARE_WE_HOME && !url.startsWith('portfolio') ? './' + url : url;
     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
   }
