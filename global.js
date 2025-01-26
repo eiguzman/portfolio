8820b54e901c205384177740f74cf39f38e73a15
eiguzman <eiguzman@ucsd.edu>
2025-01-25 21:39:28 -0800

    3.2 attempt

--- global.js
+++ global.js
@@ -3,3 +3,21 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+    { url: 'https://eiguzman.github.io/portfolio/', title: 'Home' },
+    { url: '../projects/index.html', title: 'Projects' },
+    { url: '../contact/index.html', title: 'Contact' },
+    { url: '../contact/cv.html', title: 'CV' },
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
