e2f647f0fc7126593b783dbd49b27a780cd14562
eiguzman <eiguzman@ucsd.edu>
2025-01-25 22:10:05 -0800

    updated global.js

--- global.js
+++ global.js
@@ -6,10 +6,10 @@ function $$(selector, context = document) {
 
 let pages = [
     { url: 'https://eiguzman.github.io/portfolio/', title: 'Home' },
-    { url: 'projects/index.html', title: 'Projects' },
-    { url: 'contact/index.html', title: 'Contact' },
+    { url: 'portfolio/projects/index.html', title: 'Projects' },
+    { url: 'portfolio/contact/index.html', title: 'Contact' },
     { url: 'https://github.com/eiguzman', title: 'GitHub' },
-    { url: 'contact/cv.html', title: 'CV' },
+    { url: 'portfolio/contact/cv.html', title: 'CV' },
   ];
 
 let nav = document.createElement('nav');
