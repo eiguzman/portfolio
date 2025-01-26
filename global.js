02f94a0b7e93bdef74a77af8a5353b7b67152003
eiguzman <eiguzman@ucsd.edu>
2025-01-25 20:31:10 -0800

    testing scripts

--- global.js
+++ global.js
@@ -2,12 +2,19 @@ console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
-}
+};
 
-let navLinks = $$("nav a")
+let navLinks = $$("nav a");
 
 let currentLink = navLinks.find(
     (a) => a.host === location.host && a.pathname === location.pathname
   );
 
 currentLink?.classList.add('current');
+
+// let pages = [
+//     { url: 'https://eiguzman.github.io/portfolio/', title: 'Main' },
+//     { url: '../portfolio/projects/index.html', title: 'Projects' },
+//     { url: '../portfolio/contact/index.html', title: 'Contact' },
+//     { url: '../portfolio/contact/cv.html', title: 'CV' },
+//   ];
