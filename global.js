df5cabaf7a4d27a3c1a9bd8c30445742dfd56799
eiguzman <eiguzman@ucsd.edu>
2025-01-25 23:16:33 -0800

    updated global.js

--- global.js
+++ global.js
@@ -8,30 +8,51 @@ let pages = [
     { url: 'https://eiguzman.github.io/portfolio/', title: 'Home' },
     { url: 'portfolio/projects/index.html', title: 'Projects' },
     { url: 'portfolio/contact/index.html', title: 'Contact' },
-    { url: 'https://github.com/eiguzman', title: 'GitHub' },
+    { url: 'https://github.com', title: 'GitHub' },
     { url: 'portfolio/contact/cv.html', title: 'CV' },
-  ];
+];
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const currentUrl = location.href;
+
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+
+    if (currentUrl === 'https://github.io/portfolio/' && url === 'portfolio/projects/index.html') {
+        url = 'https://github.io/portfolio/projects/index.html';
+    } else if (currentUrl === 'https://github.io/portfolio/projects/index.html' && url === 'portfolio/contact/index.html') {
+        url = 'https://github.io/portfolio/contact/index.html';
+    }
+
     if (!ARE_WE_HOME && !url.startsWith('http') && url.startsWith('portfolio')) {
         url = '/' + url;
     } else if (!ARE_WE_HOME && !url.startsWith('http')) {
         url = '../' + url;
     }
+
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
     nav.append(a);
-    
+
     a.classList.toggle(
         'current',
         a.host === location.host && a.pathname === location.pathname
-      );
-  }
+    );
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+              <!-- TODO add <option> elements here -->
+          </select>
+      </label>`
+);
