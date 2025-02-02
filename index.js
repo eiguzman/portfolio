975091cc66330b935b841fc749cb663a69018cd5
eiguzman <eiguzman@ucsd.edu>
2025-02-01 17:38:45 -0800

    updated index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
-renderProjects(latestProjects, projectsContainer, 'h2');
+renderProjects(latestProjects, projectsContainer, 'h3');
 const githubData = await fetchGitHubData('eiguzman');
 const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
